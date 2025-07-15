1370d06055a29107b1ac42e2b71fd568af221d88
Dario <dario.davp@gmail.com>
2025-07-14 21:48:45 -0400

    I added the code that chatGPT is suggesting

---
+++ Chat_gpt_codes.py
@@ -0,0 +1,92 @@
+import pygame
+import sys
+import random
+
+pygame.init()
+
+# Screen setup
+WIDTH, HEIGHT = 800, 500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Sorting Visualizer")
+clock = pygame.time.Clock()
+
+# Colors
+WHITE = (255, 255, 255)
+BLUE = (100, 149, 237)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+
+# Bar data
+NUM_BARS = 60
+data = []
+
+def generate_data():
+    return [random.randint(10, HEIGHT - 60) for _ in range(NUM_BARS)]
+
+data = generate_data()
+
+def draw_bars(data, highlight_indices=None):
+    screen.fill(WHITE)
+    bar_width = WIDTH // len(data)
+
+    for i, height in enumerate(data):
+        x = i * bar_width
+        y = HEIGHT - height
+        color = RED if highlight_indices and i in highlight_indices else BLUE
+        pygame.draw.rect(screen, color, (x, y, bar_width - 2, height))
+
+    draw_button()  # Redraw button on top
+    pygame.display.update()
+
+# Button
+BUTTON_RECT = pygame.Rect(WIDTH - 120, 10, 100, 30)
+
+def draw_button():
+    pygame.draw.rect(screen, GRAY, BUTTON_RECT)
+    font = pygame.font.SysFont(None, 24)
+    text = font.render("Reset", True, BLACK)
+    screen.blit(text, (BUTTON_RECT.x + 20, BUTTON_RECT.y + 5))
+
+# Bubble sort animation (generator)
+def bubble_sort(data):
+    n = len(data)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if data[j] > data[j + 1]:
+                data[j], data[j + 1] = data[j + 1], data[j]
+                draw_bars(data, highlight_indices=[j, j + 1])
+                yield True  # Pause after each swap
+
+# Main loop
+sorting = False
+sort_generator = None
+running = True
+
+while running:
+    if sorting:
+        try:
+            next(sort_generator)
+            clock.tick(60)
+        except StopIteration:
+            sorting = False
+    else:
+        draw_bars(data)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if BUTTON_RECT.collidepoint(event.pos):
+                data = generate_data()
+                sorting = False
+                sort_generator = None
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                sorting = True
+                sort_generator = bubble_sort(data)
+
+pygame.quit()
+sys.exit()
--- Sorting_visualizer.py
+++ Sorting_visualizer.py
@@ -10,14 +10,16 @@ WIDTH= 800
 #setting up the display screen
 screen= pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sorter visualizer")
-screen.fill("Grey")
+screen.fill("Black")
 
 #creating the clock to set FPS
 clock=pygame.time.Clock()
 
+text_font=pygame.font.Font(None,12)
+#Function that generates random data
 def get_random_data(n=50):
       return [random.randint(10,HEIGHT-10) for _ in range(n)]
-
+#Function that draws the bars on the display
 def draw_bars(surface, data):
       bar_width= WIDTH//len(data)
 
@@ -25,15 +27,21 @@ def draw_bars(surface, data):
             x= i*bar_width
             y=HEIGHT-value
             # pygame.draw.rect(surface, color, (x, y, width, height))
-            pygame.draw.rect(surface, "Blue", (x,y, bar_width-2, value))
+            pygame.draw.rect(surface, "Green", (x,y, bar_width-2, value))
 
 
+buttons_surface= pygame.surface.Surface((200,500))
+buttons_surface.fill("Grey")
 
-
+#Generate radom data to create the bars
 data=get_random_data()
 
 while True:
+      screen.blit(buttons_surface,(600,0))
+
+      #draw the bars based on the data randomly generated
       draw_bars(screen, data)
+
 
 
       for event in pygame.event.get():      
