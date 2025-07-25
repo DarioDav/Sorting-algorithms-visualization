1348c822ee13389f1ed23354c2573626e6324e2e
Dario <dario.davp@gmail.com>
2025-07-25 14:26:03 -0400

    Finished the multiple ball animation

--- Bouncing_multiple_balls/Ball_class.py
+++ Bouncing_multiple_balls/Ball_class.py
@@ -1,22 +1,19 @@
 class Circle():
-      def __init__(self, x, y, radius, color=(0, 0, 0)):
+      def __init__(self, x, y, radius, color=(0, 0, 0), x_velocity= 10, y_velocity = 10):
             self.x = x
             self.y = y
             self.radius = radius
             self.color = color
+            self.x_velocity = x_velocity
+            self.y_velocity = y_velocity
       
-      def get_center(self):
-            return (self.x, self.y)
-      
-      def get_radius(self):
-            return self.radius
-      
-      def set_center(self, x, y):
-            self.x = x
-            self.y = y
-      
-      def set_radius(self, radius):
-            self.radius = radius
-      def get_color(self):
-            return self.color
+      def move(self, WIDTH, HEIGHT):
+        self.x += self.x_velocity
+        self.y += self.y_velocity
+
+        # Bounce on edges
+        if self.x - self.radius <= 0 or self.x + self.radius >= WIDTH:
+            self.x_velocity *= -1
+        if self.y - self.radius <= 0 or self.y + self.radius >= HEIGHT:
+            self.y_velocity *= -1
       
--- Bouncing_multiple_balls/Bouncing_multiple_balls.py
+++ Bouncing_multiple_balls/Bouncing_multiple_balls.py
@@ -5,43 +5,43 @@ import random
 import Ball_class
 
 pygame.init()
-
+#Display settings
 WIDTH, HEIGHT= 800,600
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Bouncing balls")
 screen.fill('Grey')
-
 clock = pygame.time.Clock()
+
+# Initial ball settings
 circles_list= []
 
-r,g,b= random.randint(0,255), random.randint(0,255), random.randint(0,255)
-size= random.randint(50,100)
-
-
-x_velocity=15
-y_velocity =10 
-x,y= None,None
-
-circ_dict= { "r_value":r, "g_value":g, "b_value":b}
-
 #While loop for window
-
 running= True
-circle=False
 while running:
+      
+      
+      #stopping the program if you press quit
       for event in pygame.event.get():
+            
+      
+      # Create a new circle only when the left mouse button is pressed down (not held)
+            if event.type == pygame.MOUSEBUTTONDOWN :
+                  x, y = event.pos
+                  size = random.randint(50, 100)
+                  color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                  circles_list.append(Ball_class.Circle(x, y, size, color))
+            
+      screen.fill("Grey")
+      for ball in circles_list:
+            
+            ball.move(WIDTH, HEIGHT)
+            pygame.draw.circle(screen, ball.color, (ball.x, ball.y), radius=ball.radius)
+            #update position
+            
+                 
             if event.type == pygame.QUIT:
                   running = False
-      if event.type == pygame.MOUSEBUTTONDOWN:
-                 pos= pygame.mouse.get_pos()
-                 x,y= pos
-      if circle:
-            pygame.draw.circle(screen, (r,g,b), (400,300), size)
-           
-                  
-                  
-      
       #set the framerate
       clock.tick(60)
       pygame.display.update()
