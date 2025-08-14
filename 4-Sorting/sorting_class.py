40b1b195544e354ed05a9e961499c1b6b604ad10
Dario <dario.davp@gmail.com>
2025-08-13 22:31:21 -0400

    added sorting functionality with example and button class

---
+++ 4-Sorting/sorting_class.py
@@ -0,0 +1,10 @@
+class sorting_class:
+      def __init__(self, values, colors):
+            self.values = values
+            self.colors = colors
+
+      def select_sort(self):
+            n = len(self.values)
+      
+      def insert_sort(self):
+            n = len(self.values)
